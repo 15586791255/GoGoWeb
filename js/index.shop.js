193554848f2f3160ee93f59238fe0394da8b9f55
15586791255 <15586791255@163.com>
2018-03-07 17:05:34 +0800

    Initial commit

---
+++ js/index.shop.js
@@ -0,0 +1,123 @@
+function bindedbackbtn() {
+    $(".back-btn").click(function(){
+        javascript:history.back(-1);
+    });
+}
+
+$(function() {
+    inithome();
+    bindedbackbtn();
+    initNavList();
+    initNav();
+    $("#hot").addClass("nav-btn-select");
+    initNavList("hot");
+});
+
+function inithome() {
+    var data = getSessionStorage("userInfo");
+    var html = document.getElementById("home").innerHTML;
+    var source = '';
+    var nologin = '';
+    var logined = '';
+    var shop = document.getElementById("shop").innerHTML;
+
+    if (data == undefined || data == null || data == '') {
+        var html = document.getElementById("home").innerHTML;
+        var source = html.replace(reg, function (node, key) { return { 
+            'username': '点击登录',
+            'coin': '',
+            'carcount': '0',
+            'avatar': testUrl + 'images/user/def.png'
+          }[key]; });
+        var nologin = document.getElementById("nologin").innerHTML;
+        $("#Home").append(source);
+        $(".user-info").append(nologin);
+    } else {
+        initlogined(data);
+    }
+
+    $("#Home").append(shop);
+    var height = $(window).height() - $(".user-info").outerHeight(true) - $("#title").outerHeight(true) - $(".ShopList").outerHeight(true);
+    height = height + "px";
+    $(".HomeList").css("height", height);
+}
+
+function bindedLoginShow() {
+    $(".login-page").addClass("login-page-show");
+    $(".login-page").css("transform", "translateX(0)");
+    $("#mask").addClass("huiIN-show");
+}
+
+function bindLoginHide() {
+    $(".login-page").removeClass("login-page-show");
+    $(".login-page").css("transform", "translateX(-100%)");
+    $("#mask").removeClass("huiIN-show");
+}
+
+function initNav() {
+    $("div.nav-btn").click(function(){
+        $("div.nav-btn").each(function () {
+            if ($(this).hasClass("nav-btn-select")) {
+                $(this).removeClass("nav-btn-select");
+            }
+        });
+        $(this).addClass("nav-btn-select");
+        initNavList($(this).attr("id"));
+    });
+}
+
+function initNavList(_id) {
+    $(".HomeList").html("");
+    $.ajax({
+        type: 'GET',
+        url: "api/mall/goods/",
+        timeout: 5000,
+        dataType: 'JSON',
+        data: {
+            'tp': _id
+        },
+        beforeSend: function(xhr){
+            xhr.setRequestHeader('pt', 'web');
+            xhr.setRequestHeader('app_key', 'test_key');
+        },
+        success: function(data) {
+            if (data.code == 200) {
+                var data = data.data;
+                var html = '';
+                var arrlen = data.items.length;
+                if (arrlen > 0) {
+                    for (var index in data.items) {
+                        html += '<div class="list">';
+                        html += '<div class="border">';
+                        html += '<div class="img-box">';
+                        html += '<div class="img" style="background: url(&quot;' + data.items[index].cover + '&quot;) center center / cover no-repeat rgb(255, 255, 255);">';
+                        html += '</div>';
+                        html += '</div>';
+                        html += '<div class="name">' + data.items[index].title + '</div>';
+                        html += '<div class="money">' + data.items[index].coin + '<span>竞币</span>';
+                        html += '</div>';
+                        html += '</div>';
+                        html += '</div>';
+                    }
+                    $(".HomeList").html(html);
+                    setTimeout(function(){
+                        initBindButton();
+                    }, 200);
+                } else {
+                    Toast("暂时没有此类商品", 1000);
+                }
+            } else if (data.code == 500) {
+
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            console.log('error ' + textStatus + " " + errorThrown);
+        }
+    });
+}
+
+function initBindButton() {
+    $(".list").unbind().click(function() {
+         $("#am-modal-container").show();
+    });
+}
---
+++ js/index.user.js
@@ -0,0 +1,179 @@
+var countdown = 60;
+
+function getUserInfo() {
+    console.log(getSessionStorage(sessionStorageJsonKey[0]));
+    $.ajax({
+        url: "api/core/user",
+        dataType: 'JSON',
+        timeout: 5000,
+        type: "GET",
+        beforeSend: function(xhr){
+            sendHeader(xhr, getSessionStorage(sessionStorageJsonKey[0]));
+        },
+        success: function(data){
+            if (data.code == 200) {
+                setSessionStorage(sessionStorageJsonKey[1], data.data);
+                initlogined(data.data);
+            } else {
+                Toast("连接超时", 1000);
+            }
+            console.log(data.data);
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            console.log('error ' + textStatus + " " + errorThrown);
+        }
+    });
+}
+
+function login() {
+    var inputNumber = checkInput($("#phone-input"), "请输入手机号");
+    if (inputNumber != undefined && inputNumber != "") {
+        var code = checkInput($("#code"), "请输入验证码");
+        if (code != undefined && inputNumber != "") {
+            $.ajax({
+                url: "api/account/sms_login",
+                dataType: 'JSON',
+                timeout: 5000,
+                type: "POST",
+                data: {
+                    "tel": inputNumber,
+                    "code": code
+                },
+                beforeSend: function(xhr){
+                    sendHeaderMin(xhr);
+                },
+                success: function(data){
+                    if (data.code == 200) {
+                        setSessionStorage(sessionStorageJsonKey[0], data.data);
+                        $.ajax({
+                            url: "api/core/user",
+                            dataType: 'JSON',
+                            timeout: 5000,
+                            type: "GET",
+                            beforeSend: function(xhr){
+                                sendHeader(xhr, getSessionStorage(sessionStorageJsonKey[0]));
+                            },
+                            success: function(data){
+                                if (data.code == 200) {
+                                    setSessionStorage(sessionStorageJsonKey[1], data.data);
+                                    location.reload();
+                                } else {
+                                    Toast("连接超时", 1000);
+                                }
+                                console.log(data.data);
+                            },
+                            error: function(jqXHR, textStatus, errorThrown){
+                                console.log('error ' + textStatus + " " + errorThrown);
+                            }
+                        });
+                    } else {
+                        Toast("手机验证码错误或者已过期", 1000);
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown){
+                    Toast("连接超时", 1000);
+                    console.log('error ' + textStatus + " " + errorThrown);
+                }
+            });
+        }
+    }
+}
+
+function refreshUserInfo() {
+	var loginInfo = getSessionStorage(sessionStorageJsonKey[0]);
+	if (loginInfo != null) {
+        $.ajax({
+            url: "api/account/token",
+            dataType: 'JSON',
+            timeout: 5000,
+            type: "POST",
+            data: {
+                "refresh_token": loginInfo.refresh_token
+            },
+            beforeSend: function(xhr){
+                sendHeader(xhr, loginInfo);
+            },
+            success: function(data){
+                if (data.code == 200) {
+                    setSessionStorage(sessionStorageJsonKey[0], data.data);
+                    getUserInfo();
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                Toast("连接超时", 1000);
+                console.log('error ' + textStatus + " " + errorThrown);
+            }
+        });
+	}
+	
+}
+
+function initlogined(_data) {
+    $("#login-page").remove();
+    $(".user-info").remove();
+    if (_data.avatar == null || _data.avatar == undefined || _data.avatar == '') {
+        _data.avatar = testUrl + 'images/user/def.png';
+    }
+    var html = document.getElementById("home").innerHTML;
+    var source = html.replace(reg, function (node, key) { return { 
+        'username': _data.username,
+        'coin': _data.coin,
+        'carcount': '0',
+        'avatar': _data.avatar
+      }[key]; });
+    var logined = document.getElementById("logined").innerHTML;
+    var sourceLogined = logined.replace(reg, function (node, key) { return { 
+        'username': _data.username,
+        'coin': _data.coin,
+        'avatar': _data.avatar
+      }[key]; });
+    $("#Home").prepend(source);
+    $(".user-info").append(sourceLogined);
+    $(".setBtn").click(function() {
+        $("#am-modal-container").show();
+    });
+}
+
+function signOut() {
+    removeSession(sessionStorageJsonKey[0]);
+    removeSession(sessionStorageJsonKey[1]);
+    setTimeout(function(){
+        window.location.href = $("base").attr("href") + testUrl + "/index.html";
+    }, 200);
+}
+
+
+function getCode() {
+    if (countdown != 60) {
+        Toast("请稍候再试", 1000);
+        return;
+    }
+    var inputNumber = checkInput($('#phone-input'), "请输入正确的手机号");
+    if (inputNumber != undefined && inputNumber != "") {
+        $.ajax({
+            url: "api/account/sms",
+            dataType: 'JSON',
+            timeout: 5000,
+            type: "POST",
+            data: {
+                "tel":inputNumber
+            },
+            beforeSend: function(xhr){
+                xhr.setRequestHeader('pt', 'web');
+                xhr.setRequestHeader('app_key', 'test_key');
+            },
+            success: function(data){
+                settime($(".code-btn"), "code-btn-ing");
+                if (data.code == 200) {
+                    console.log(1);
+                } else if (data.code == 500) {
+                    console.log(2);
+                }
+                console.log(data.msg);
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                console.log('error ' + textStatus + " " + errorThrown);
+            }
+        });
+    }
+}
