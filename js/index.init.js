7efb2fa0f39f42eeb155b5e45527ee5257a1cbab
15586791255 <15586791255@163.com>
2018-04-12 09:06:07 +0800

    修复时间显示

--- js/index.init.js
+++ js/index.init.js
@@ -304,8 +304,8 @@ function timestampToDate(timestamp) {
     M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '月';
     D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + '日 ';
     h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours())  + ':';
-    m = date.getMinutes();
-    s = date.getSeconds();
+    m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())  + ':';
+    s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
     return Y + M + D + h + m + s;
 }
 
